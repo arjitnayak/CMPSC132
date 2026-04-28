82e0e25e80f0833576225fa9b9db8d073dcd235c
Arjit Nayak <arjitnayak@Arjits-MacBook-Air.local>
2026-04-28 01:05:28 -0400

    Finished Adding Unlimited Attempts

--- final_project_CMPSC132.py
+++ final_project_CMPSC132.py
@@ -2,16 +2,12 @@ import random
 import math
 
 def number_guessing_game():
+    print("\nGenerating Number...")
     num = random.randint(1,100)
+    print("Number Generated\n")
     guess = -math.inf
     attempts = []
-    print("Welcome to the Single Player Number Guessing Game")
-    print("Rules are Simple:")
-    print("1. The Computer Generates a Random Number Between 1-100 (INCLUSIVE) and You Must Guess It")        
-    print("2. Based On Your Input, The Computer Will Give You Feedback (\"Too High\" or \"Too Low\")")
-    print("3. The Game Will End As Soon As You Get The Correct Number and Will Display The Number of Attempts It Took You")
-    print("4. Last But Not Least, Have Fun\n")
-    while num != guess:
+    while ((num != guess) and (len(attempts) < 5)):
         try:
             guess = int(input("What's Your Guess: "))
             if (guess > num):
@@ -21,8 +17,24 @@ def number_guessing_game():
             attempts.append(guess)
         except ValueError:
             print("Please Enter an Integer")
-    print(f"\nCongratulations! The Number Was {guess}")
-    print(f"You Took {len(attempts)} Attempts To Guess The Correct Number")
+    if (num != guess):
+        print("\nYou Lost :(")
+        print(f"The Number Was {num}")
+    else:
+        print(f"\nCongratulations! The Number Was {guess}")
+        print(f"You Took {len(attempts)} Attempts To Guess The Correct Number\n")
+        
 
 if __name__ == "__main__":
-    number_guessing_game()
+    print("Welcome to the Single Player Number Guessing Game")
+    print("Rules are Simple:")
+    print("1. The Computer Generates a Random Number Between 1-100 (INCLUSIVE) and You Must Guess It In 5 Attempts")        
+    print("2. Based On Your Input, The Computer Will Give You Feedback (\"Too High\" or \"Too Low\")")
+    print("3. The Game Will End As Soon As You Get The Correct Number and Will Display The Number of Attempts It Took You")
+    print("4. Last But Not Least, Have Fun\n")
+    status = input("Do You Want To Play? (Y/N): ")
+    while status == "Y":
+        number_guessing_game()
+        status = input("\nDo You Want To Play Another Round? (Y/N): ")
+    print("\nThank You For Playing the Single Player Number Guessing Game")
+    print("Make Sure To Come Back To Play More")
